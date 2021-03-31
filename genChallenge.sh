77dba6a818efe71eaaae9877de4d85a292180e35
XNUConner <72773002+XNUConner@users.noreply.github.com>
2021-03-30 20:46:52 -0500

    Modify bash script to automate README additions

--- genChallenge.sh
+++ genChallenge.sh
@@ -1,4 +1,5 @@
 #!bin/bash
+# Generates templates to automate adding CTF challenges
 if [ "$#" -ne 3 ]; then
 	echo "Usage: $0 \"challenge name\" urlnumber points"
 	exit
@@ -7,3 +8,5 @@ NAME=$(echo "$1" | tr ' ' '_' | tr '[:upper:]' '[:lower:]' | tr -d '?')
 SUBDIR=challenges/$NAME
 mkdir "$SUBDIR"
 echo "# [$1](https://ctflearn.com/challenge/$2) ($3)" | tee "$SUBDIR/challenge.md" "$SUBDIR/solution.md" > /dev/null 2>&1
+N="$(ls -1q challenges/ | wc -l)"
+echo "$N. [$1](https://github.com/XNUConner/CTFLearn/tree/master/challenges/$NAME) <br />" >> README.md
